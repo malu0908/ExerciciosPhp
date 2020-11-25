59c6a009d21a141baba27de43a2a79a1ff8fa250
Maria Luiza Fernandes <40873568+malu0908@users.noreply.github.com>
2020-11-25 13:27:42 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -29,13 +29,31 @@ class Resolucao implements TextWrapInterface {
     $words = [];
     $currentPosition = 0;
 
+    /*
+     * Verifica se a string a ser dividida não é vazia.
+     * Verifica se o tamanho máximo de cada substring é maior que zero.
+     */
     if ($length > 0 && strlen($text) > 0) {
+      
+      /*
+       * Percorre a string a ser dividida.
+       */
       for ($upIndex = 0; $upIndex < strlen($text);) {
         $downIndex = $upIndex + $length;
 
         $limit = $length;
+        
+        /*
+         * Percorre a string da posição atual até o limite definido.
+         */
         for ($j = 0; $j < $limit; $j++) {
           if ($upIndex + $j < strlen($text)) {
+            
+            /*
+             * Verifica se o caracter atual é especial.
+             * Caso seja é adicionado uma posição no limite,
+             * pois um caracter especial ocupa duas posições na string.
+             */
             if (preg_match('/^[ç´`~^]+/', $text[$upIndex + $j])) {
               $j++;
               $limit++;
@@ -43,6 +61,9 @@ class Resolucao implements TextWrapInterface {
 
           }
 
+          /*
+           * Atualiza $downIndex que marca a posição final da substring atual.
+           */
           if ($upIndex + $j < strlen($text) - 1) {
             if ($text[$upIndex + $j + 1] == ' ') {
               $downIndex = $upIndex + $j + 1;
@@ -56,15 +77,26 @@ class Resolucao implements TextWrapInterface {
         }
 
         $newWord = "";
+
+        /*
+         * Cria uma string com os caracteres contidos entre
+         * o limite inferior $upIndex e o limite superior $downIndex.
+         */
         for ($j = 0; $upIndex + $j < $downIndex; $j++) {
           if ($upIndex + $j < strlen($text)) {
             $newWord[$j] = $text[$upIndex + $j];
           }
         }
 
+        /*
+         * Adiciona a substring no array de retorno.
+         */
         $words[$currentPosition] = $newWord;
         $currentPosition++;
 
+        /*
+         * Atualiza o indíce de limite inferior para o próximo caracter.
+         */
         if ($downIndex < strlen($text)) {
           $sum = 0;
           $aux = $downIndex;
